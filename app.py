f1c6288e32f032cb53446efef3d962abb87a58d8
Vivek Kumar Chaurasiya <90383948+vivek849@users.noreply.github.com>
2025-06-06 12:32:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,6 +104,6 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
